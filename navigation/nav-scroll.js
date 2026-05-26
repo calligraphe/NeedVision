66448dc84f3c26edeed88ead4358c58e0c552164
Alisher <aoxuaio@gmail.com>
2026-05-26 15:11:48 +0500

    tune(nav): сжатие 300vw (было 215vw)

--- navigation/nav-scroll.js
+++ navigation/nav-scroll.js
@@ -190,7 +190,7 @@ function bootNavScroll() {
       scrollTrigger: {
         trigger: "body",
         start: "top top",
-        end: "+=215vw",
+        end: "+=300vw",
         scrub: 1
       },
       onUpdate: () => {
